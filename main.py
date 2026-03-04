ab490e9b631dfb430222ff2679aee52ca54ede67
Rudra-Rathi <rudra.rathi1507@gmail.com>
2026-03-04 22:33:06 +0530

    updated code

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from src.transcript_extractor import get_transcript
 from src.sentiment_model import analyze_sentiment
 
-
+# this is the code i push today, it is working for putting youtube video link and getting the transcript and sentiment analysis of the transcript, it will print the first 10 sentences with their sentiment and confidence score.
 def main():
 
     url = input("Enter YouTube URL: ")
+    print("Extracting transcript...")
+
 
     transcript = get_transcript(url)
+    print("Analyzing sentiment...")
 
     sentiments = analyze_sentiment(transcript)
 
